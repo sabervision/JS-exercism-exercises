2ea7ddb497523c61dccef385568e0be7741503c6
sabervision <brhernandez314@gmail.com>
2016-09-23 03:19:52 -0700

    updated hello_world

--- Hello_World/hello_world.js
+++ Hello_World/hello_world.js
@@ -1,3 +1,9 @@
 // get the user's name
-
 var name = window.prompt("Greetings! What's your name?");
+
+// Display hello greeting
+if (name === '') {
+	window.alert("Hello World!");
+} else {
+	window.alert(`Hello, ${name}`);
+}
